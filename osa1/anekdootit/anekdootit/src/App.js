8796c9fc4ebefda86f8318feed8d02a95c26daaf
Teemu Romo <teemu.romo@aalto.fi>
2022-09-08 14:13:22 +0300

    osa1 valmis

---
+++ osa1/anekdootit/anekdootit/src/App.js
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+const Anecdotesoftheday = () => (<h1>Anecdots of the day</h1>)
+const Anecdotmost = () => (<h1>Anecdot with the most votes</h1>)
+
+const Button = ({ click, name }) => (
+  <button onClick={click}>
+    {name}
+  </button>
+)
+const Anecdotes = ({selected, anecdotes}) => (<div>{anecdotes[selected]}</div>)
+const Votes = ({selected, votes}) => (<div>has {votes[selected]} votes</div>)
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const random = () => setSelected(Math.floor(Math.random() * (7)))
+  const [array, setVote] = useState([0,0,0,0,0,0,0])
+  const vote = () => {
+    const newVotes = [...array]
+    newVotes[selected] += 1
+    setVote(newVotes)
+   // console.log(array)
+  }
+
+  return (
+    <div>
+      <Anecdotesoftheday />
+      <Anecdotes anecdotes={anecdotes} selected={selected}/>
+      <Votes votes={array} selected={selected}/>
+      <Button click={vote} name='vote' />
+      <Button click={random} name='next'/>
+      <Anecdotmost />
+      <Anecdotes anecdotes={anecdotes} selected={array.indexOf(Math.max(...array))}/>
+      <Votes votes={array} selected={array.indexOf(Math.max(...array))}/>
+    </div>
+  )
+}
+
+export default App
