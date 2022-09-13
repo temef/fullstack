f4df732a9d8dcc752aee177c1778a69493f5df54
Teemu Romo <teemu.romo@aalto.fi>
2022-09-13 12:51:53 +0300

    osa2 kurssitiedot step7

---
+++ osa2/kurssitiedot2/src/components/Course.js
@@ -0,0 +1,28 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    {parts.map(part =>
+    <Part key={part.id} part={part}/>
+ )}
+  </>
+  
+
+const Course = ({ course }) => {
+    return(
+        <div>
+            <Header course={course.name}/>
+            <Content parts={course.parts}/>
+        </div>
+    )
+
+}
+
+  export default Course
