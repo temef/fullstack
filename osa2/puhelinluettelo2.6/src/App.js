c6ae2be5d71e470230d7c628e6c4024a036315dd
Teemu Romo <teemu.romo@aalto.fi>
2022-09-23 15:46:47 +0300

    2.11 valmis

--- osa2/puhelinluettelo2.6/src/App.js
+++ osa2/puhelinluettelo2.6/src/App.js
@@ -1,15 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Person from './components/Person'
 import AddNew from './components/addNew'
 import Filter from './components/filter'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
